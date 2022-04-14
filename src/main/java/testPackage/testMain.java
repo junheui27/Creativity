6209dfedba25d3d7891471cbcbadf8f900a1ae27
dg1.lee <akxkdldhtm@gmail.com>
2022-04-14 12:03:53 +0900

    gradle 설정파일 추가 업로드

--- src/main/java/testPackage/testMain.java
+++ src/main/java/testPackage/testMain.java
@@ -1,8 +1,6 @@
 package testPackage;
 
 public class testMain {
-
-
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Commit Test Dowon");
